074dfc00af6c4f4c3f12d56ad04c544c4d41d480
Marcin Nowicki <pr0d1r2@gmail.com>
2022-03-29 10:42:52 +0200

    Add fake bottle for monterey

--- Formula/python@2.7.17.rb
+++ Formula/python@2.7.17.rb
@@ -10,6 +10,7 @@ class PythonAT2717 < Formula
     sha256 "accfaa922708f00afb69ab230199f96e6ecdddd248a1eca586ce1e5e5cfd732b" => :catalina
     sha256 "54d3351d6be8268b2f5017894dcc8e083811dfa3812bdb9f79f989873b9a4542" => :mojave
     sha256 "cfd5c6eeac37065d19f527bb0798a9caf1928bab3340cd545224861a3c82f219" => :high_sierra
+    sha256 "cfd5c6eeac37065d19f527bb0798a9caf1928bab3340cd545224861a3c82f219" => :monterey
   end
 
   # setuptools remembers the build flags python is built with and uses them to
